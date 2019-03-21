0ba0ed6ac6e139eab01dc52bdf21b323beab3b44
Samuel Allemang <samuel.allemang@gmail.com>
2019-03-21 19:23:53 -0400

    Initial commit. Nearly out-of-the-box Sage 9 install.

---
+++ resources/assets/scripts/main.js
@@ -0,0 +1,39 @@
+// import external dependencies
+import 'jquery';
+
+// Import everything from autoload
+import './autoload/**/*'
+
+// import local dependencies
+import Router from './util/Router';
+import common from './routes/common';
+import home from './routes/home';
+import aboutUs from './routes/about';
+
+/** Populate Router instance with DOM routes */
+const routes = new Router({
+  // All pages
+  common,
+  // Home page
+  home,
+  // About Us page, note the change from about-us to aboutUs.
+  aboutUs,
+});
+
+// Load Events
+jQuery(document).ready(() => routes.loadEvents());
+
+
+window.WebFontConfig = {
+      google: { 
+        families: [ 'Open+Sans:400,400italic,700:latin' ],
+      },
+};
+(function() {
+      var wf = document.createElement('script');
+      wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+      wf.type = 'text/javascript';
+      wf.async = 'true';
+      var s = document.getElementsByTagName('script')[0];
+      s.parentNode.insertBefore(wf, s);
+})();
