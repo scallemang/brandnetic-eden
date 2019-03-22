f085a7eeb8d4eb80241dd008f94b942df055bbbf
SC Allemang <mpsdev1@MPSs-iMac.local>
2019-03-22 13:16:34 -0400

    Updating navbar, hero styles

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,8 @@
 <header class="banner">
   <div class="container">
     {{-- <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a> --}}
-    <nav class="nav-primary">
+    {{-- <nav class="nav-primary"> --}}
+    <nav class="row flex-nowrap justify-content-between align-items-center">
       @if (has_nav_menu('primary_navigation'))
         {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
       @endif
