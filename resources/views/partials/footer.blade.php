8e4b04261e305f2a2bd2217286df257e479a4f08
Samuel Allemang <samuel.allemang@gmail.com>
2019-03-25 09:59:29 -0400

    Styling

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,24 +1,41 @@
-<footer class="content-info py-5">
-  <div class="container pb-5">
+<footer class="content-info">
+  <div class="container">
   	<div class="row">
-  		<div class="col-lg-8">
-  			<h4 class="mb-5">Another Fortune Marketing Project</h4>
-        <p>EDEN Property Specialists</p>
+  		<div class="col-md-11 offset-md-1 offset-lg-0 col-lg-8">
+  			<h4>Another Fortune Marketing Project</h4>
+        <p class="t-uppercase mb-0">EDEN Property Specialists</p>
   			<div class="row">
-          <div class="col-eden col-eden-lg-4">
+          <div class="col-eden col-eden-md-6 col-eden-lg-4">
             <p>250-317-9947<br>
             eden@fortunemarketing.ca</p>
           </div>
           <div class="v-line"></div>
-          <div class="col-eden col-eden-lg-4">
+          <div class="col-eden col-eden-md-6 col-eden-lg-4">
             <p>Open M-F, 9-5<br>
             305 Lawrence Ave, Kelowna</p>
           </div>
-  		</div>
-  		<div class="col-lg-4">
-  			
-  		</div>
-  	</div>
+          <div class="col-sm-12">
+            <div class="social-icons">
+              <ul>
+                <li><a href="https://facebook.com" target="_blank"><img src="@asset('images/Facebookeden_logo.png')" alt="Facebook logo"></a></li>
+                <li><a href="https://twitter.com" target="_blank"><img src="@asset('images/Twittereden_logo.png')" alt="Twitter logo"></a></li>
+                <li><a href="https://instagram.com" target="_blank"><img src="@asset('images/Instagrameden_logo.png')" alt="Instagram logo"></a></li>
+              </ul>
+            </div>
+          </div>
+    		</div>
+      </div>
+      <div class="col-md-11 offset-md-1 offset-lg-0 col-lg-4">
+        <div class="partner-logos">
+          <ul>
+            <li><a href="https://facebook.com" target="_blank"><img src="@asset('images/Asset_1eden_logo.png')" alt="Facebook logo"></a></li>
+            <li><a href="https://twitter.com" target="_blank"><img src="@asset('images/Asset_2eden_logo.png')" alt="Twitter logo"></a></li>
+            <li><a href="https://instagram.com" target="_blank"><img src="@asset('images/Asset_3eden_logo.png')" alt="Instagram logo"></a></li>
+          </ul>
+        </div>
+      </div>
+		</div>
+	</div>
     {{-- @php dynamic_sidebar('sidebar-footer') @endphp --}}
-  </div>
+
 </footer>
