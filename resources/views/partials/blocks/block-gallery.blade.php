6c45ef644ae026482d711cbe2c1960126d9ec9c4
Samuel Allemang <samuel.allemang@gmail.com>
2019-07-16 20:03:09 -0400

    Update to gallery template

--- resources/views/partials/blocks/block-gallery.blade.php
+++ resources/views/partials/blocks/block-gallery.blade.php
@@ -71,18 +71,20 @@
               }
             @endphp
             <div class="col-lg-3">
-              <a @if( 'file' == $linkType ) data-file="{{ $linkUrl }}" @else href="{{ $linkUrl }}" @endif ><img src="{{ $image['sizes']['medium'] }}" alt="{{ $image['alt'] }}" class="img-fluid"></a>
+              <a href="#" @if( 'file' == $linkType ) data-toggle="modal" data-target="#modal-{{ $uniqueId }}" @else href="{{ $linkUrl }}" @endif ><img src="{{ $image['sizes']['medium'] }}" alt="{{ $image['alt'] }}" class="img-fluid"></a>
             </div>
             @if( 'file' == $linkType )
-              @php $modals[ $uniqueId ] = $linkUrl @endphp
+              @php 
+                $modals[] = array( 
+                  'id'  => $uniqueId,
+                  'url' => $linkUrl,
+                );
+              @endphp
             @endif
           @endforeach
         </div>
       @endif
       {{-- Modals --}}
-      @if( !empty( $modals ) )
-        
-      @endif
     {{-- End of WP --}}
     </div>
   @endif
@@ -111,3 +113,46 @@
   @endif
   {{-- End of Grid --}}
 </section>
+
+@if( !empty( $modals ) )
+  @foreach( $modals as $modal )
+    <div class="modal fade" id="modal-{{ $modal['id'] }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            
+
+
+            <div class="reveal-overlay" style="display: block;">
+              <div class="reveal" id="floorplan-modal" data-reveal="88ckrs-reveal" role="dialog" aria-hidden="false" data-yeti-box="floorplan-modal" data-resize="floorplan-modal" tabindex="-1" style="display: block; top: 51px;" data-events="resize">
+                  <div class="embed-responsive" style="padding-bottom:110%">
+                    <object id="pdf-viewer" data="{{ $modal['url'] }}" type="application/pdf" width="100%" height="100%" internalinstanceid="6"></object>
+                  </div>
+                  <div class="pdf-button">
+                    <a target="_blank" class="js-pdf-btn button" href="{{ $modal }}">Download PDF</a>
+                  </div>
+            <button class="close-button" data-close="" aria-label="Close modal" type="button">
+            <span aria-hidden="true">×</span>
+            </button>
+            </div></div>
+
+
+
+
+
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  @endforeach
+@endif
